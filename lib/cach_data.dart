b5589c7f6caba142769335162fbe71defe5efbae
smilingmajid <majid.gha3mi.p@gmail.com>
2025-04-06 02:12:17 -0800

    cach data file create

--- lib/cach_data.dart
+++ lib/cach_data.dart
@@ -1,5 +1,4 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library cach_data;
+
+export 'src/cache_manager.dart';
+export 'src/data_service.dart';
